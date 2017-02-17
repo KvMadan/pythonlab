9cd6e5d659f9e9a51d21623a6c2582f3c00be66d
justinmichaels006 <justin@couchbase.com>
2017-02-17 13:54:32 -0800

    Add mobile beer sample

--- MobileBeerSample.py
+++ MobileBeerSample.py
@@ -5,6 +5,7 @@ from couchbase.bucket import Bucket
 import requests
 from couchbase.views.iterator import View
 from collections import Counter
+import numpy as np
 
 cb = Bucket('couchbase://10.112.151.101/beer-sample?operation_timeout=30')
 cb2 = Bucket('couchbase://10.112.151.101/beers?operation_timeout=30')
@@ -32,6 +33,7 @@ print (avg_value)
 modedata = Counter(avg_track)
 #print (modedata.most_common())   # Returns all unique items and their counts
 print (modedata.most_common(1))  # Returns the highest occurring item
+print (np.percentile(avg_track, 95))
 
 i = 0
 start = 0
@@ -54,5 +56,6 @@ print(avg_value)
 modedata = Counter(avg_track)
 #print(modedata.most_common())   # Returns all unique items and their counts
 print(modedata.most_common(1))  # Returns the highest occurring item
+print (np.percentile(avg_track, 95))
 
 cb.closed
