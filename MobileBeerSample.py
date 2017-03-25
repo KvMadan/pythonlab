7b47b1ae852d584ead71a032c96984e971730fda
justinmichaels006 <justin@couchbase.com>
2017-03-25 08:32:41 -0700

    Added N1QL Test with SSL and RESTful CB

--- MobileBeerSample.py
+++ MobileBeerSample.py
@@ -9,8 +9,10 @@ import numpy as np
 
 CBS="10.112.151.101"
 SG="10.112.151.101"
+CBG="10.112.151.1"
 cb = Bucket('couchbase://10.112.151.101/beer-sample?operation_timeout=30')
 cb2 = Bucket('couchbase://10.112.151.101/beers?operation_timeout=30')
+thecert = "CBcert.ca"
 
 i = 0
 start = 0
@@ -65,7 +67,7 @@ for result in View(cb2, "beer", "allkeys"):
     jsonID = result.docid
     theStatement = "statement=SELECT%20*%20from%20%60beer-sample%60%20USE%20KEYS%20%22" + jsonID + "%22"
     start = time.time()
-    resp = requests.get('http://{}:8093/query?'.format(SG), data=theStatement, headers=headers)
+    resp = requests.get('https://{}:18093/query?'.format(SG), data=theStatement, headers=headers, cert=thecert)
     end = time.time()
     avg_track.extend([(end - start) * 1000])
     #time.sleep(2)
@@ -74,6 +76,24 @@ for result in View(cb2, "beer", "allkeys"):
 avg_value = sum(avg_track)/len(avg_track)
 #print(avg_track)
 print("average N1QL GET ...")
+print(avg_value)
+modedata = Counter(avg_track)
+#print(modedata.most_common())   # Returns all unique items and their counts
+print(modedata.most_common(1))  # Returns the highest occurring item
+print (np.percentile(avg_track, 95))
+
+for result in View(cb2, "beer", "allkeys"):
+    jsonID = result.docid
+    start = time.time()
+    resp = requests.get('http://{}:8888/GETit/'.format(CBG), data=jsonID, headers=headers)
+    end = time.time()
+    avg_track.extend([(end - start) * 1000])
+    #time.sleep(2)
+    i = i + 1
+
+avg_value = sum(avg_track)/len(avg_track)
+#print(avg_track)
+print("average RESTful GET ...")
 print(avg_value)
 modedata = Counter(avg_track)
 #print(modedata.most_common())   # Returns all unique items and their counts
